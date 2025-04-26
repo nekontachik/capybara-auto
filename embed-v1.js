e0b399d10f9d056acd7f89cb1636f2dfdfef2eb6
Alex <copy.recopy@gmail.com>
2025-04-26 18:22:01 +0300

    Create embed-v1.js

---
+++ embed-v1.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Promptly Embed</title>
+</head>
+<body>
+  <!-- Підключаємо локальну копію скрипта -->
+  <script async src="./embed-v1.js"></script>
+
+  <!-- Ваш чат-елемент -->
+  <promptly-app-embed
+    published-app-id="541115e0-fa76-4608-82ff-fa343022aa25"
+    chat-bubble="true"
+    host="https://trypromptly.com">
+  </promptly-app-embed>
+</body>
+</html>
